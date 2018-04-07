fafb9cfc1ba42cdad615034171e8928f24ddd381
salayhin <salayhin@gmail.com>
2018-04-07 19:02:06 +0600

    Company Model Added

--- job_portal/admin.py
+++ job_portal/admin.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from .models import BusinessType
+from .models import Company
 
 from django.contrib import admin
 
 # Register your models here.
 
 admin.site.register(BusinessType)
+admin.site.register(Company)
--- job_portal/models.py
+++ job_portal/models.py
@@ -2,8 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class BusinessType(models.Model):
     type = models.CharField(max_length=200)
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Company(models.Model):
+    business_type = models.ForeignKey(BusinessType, on_delete=models.CASCADE)
+    business = models.TextField()
+    office_address = models.CharField(max_length=200)
+    contact_person = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    total_employee = models.IntegerField()
+    year_established = models.IntegerField()
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
