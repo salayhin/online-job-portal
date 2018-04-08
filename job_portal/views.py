ee5f55ac46cd0782c1c4b103870d64f6b287ecd4
salayhin <salayhin@gmail.com>
2018-04-09 00:52:31 +0600

    View file modified

--- job_portal/views.py
+++ job_portal/views.py
@@ -1,29 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
 from .models import Contract
 from .forms import contract_form
-
+from .models import FAQ
+import pdb
 
 # Create your views here.
 
 def contract(request):
     if request.method == 'POST':
+
         form = contract_form(request.POST)
         if form.is_valid():
             form.save()
 
-    return render(request, 'job_portal/contract.html',{'form':form})
+    return render(request, 'job_portal/contract.html',{'form':contract_form})
 
-from .models import FAQ
+
 
 # Create your views here.
 
 def faq(request):
     all = FAQ.objects.all()
     return render(request, 'faq.html',{'faq':all})
+
+
 def about(request):
     return render(request,'about.html')
 
